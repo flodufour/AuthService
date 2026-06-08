98995c50946773edf08c5d979b28ba630b9cd73b
flo404x <floriandufour66@gmail.com>
2026-06-08 20:33:07 +0200

    required email

--- DTO/MeResponse.cs
+++ DTO/MeResponse.cs
@@ -1,7 +1,7 @@
 ﻿public class MeResponse
 {
     public Guid Id { get; set; }
-    public string Email { get; set; }
+    public required string Email { get; set; }
     public bool IsEmailVerified { get; set; }
     public DateTime CreatedAt { get; set; }
     public List<string> Roles { get; set; } = new();
